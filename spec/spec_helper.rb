ae6b92c9b37fa2b0342f1ddf5069170bb2094550
Ben Purinton <ben.purinton@gmail.com>
2023-07-16 15:34:10 -0800

    update spec_helper to allow open-uri solution

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,45 +28,34 @@ path_to_file = Dir.pwd + "/spec/support/currency_symbols.json"
 api_response = open(path_to_file).read.chomp
 RSpec.configure do |config|
   config.before(:each) do
-    # stub_request(:get, /api.exchangerate.host\/symbols/).
-    # with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
-    # to_return(status: 200, body: api_response, headers: {})
     stub_request(:get, /api.exchangerate.host\/symbols/).
-      with(headers: {
-          'Connection'=>'close',
-          'Host'=>'api.exchangerate.host',
-          'User-Agent'=>"http.rb/#{HTTP::VERSION}"
-           }).
-        to_return(status: 200, body: api_response, headers: {})
+      with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
+      to_return(status: 200, body: api_response, headers: {})
+
+    stub_request(:get, /api.exchangerate.host\/symbols/).
+      with(headers: {'Connection'=>'close', 'Host'=>'api.exchangerate.host', 'User-Agent'=>"http.rb/#{HTTP::VERSION}"}).
+      to_return(status: 200, body: api_response, headers: {})
   end
 end
 path_to_file = Dir.pwd + "/spec/support/cup_to_svc.json"
 cup_to_svc = open(path_to_file).read.chomp
 RSpec.configure do |config|
   config.before(:each) do
-    # stub_request(:get, /api.exchangerate.host\/convert/).
-    #   with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
-    #   to_return(status: 200, body: cup_to_svc, headers: {})
-
-    # stub_request(:get, /api.exchangerate.host\/latest/).
-    #   with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
-    #   to_return(status: 200, body: cup_to_svc, headers: {})
-
     stub_request(:get, /api.exchangerate.host\/convert/).
-    with(headers: {
-        'Connection'=>'close',
-        'Host'=>'api.exchangerate.host',
-        'User-Agent'=>"http.rb/#{HTTP::VERSION}"
-         }).
+      with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
       to_return(status: 200, body: cup_to_svc, headers: {})
 
-      stub_request(:get, /api.exchangerate.host\/latest/).
-      with(headers: {
-          'Connection'=>'close',
-          'Host'=>'api.exchangerate.host',
-          'User-Agent'=>"http.rb/#{HTTP::VERSION}"
-           }).
-        to_return(status: 200, body: cup_to_svc, headers: {})
+    stub_request(:get, /api.exchangerate.host\/latest/).
+      with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
+      to_return(status: 200, body: cup_to_svc, headers: {})
+
+    stub_request(:get, /api.exchangerate.host\/convert/).
+      with(headers: {'Connection'=>'close', 'Host'=>'api.exchangerate.host', 'User-Agent'=>"http.rb/#{HTTP::VERSION}"}).
+      to_return(status: 200, body: cup_to_svc, headers: {})
+
+    stub_request(:get, /api.exchangerate.host\/latest/).
+      with(headers: {'Connection'=>'close', 'Host'=>'api.exchangerate.host', 'User-Agent'=>"http.rb/#{HTTP::VERSION}"}).
+      to_return(status: 200, body: cup_to_svc, headers: {})
   end
 end
 
